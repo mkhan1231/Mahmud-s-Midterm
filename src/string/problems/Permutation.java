2d34805f5f21236c1b766fbdd77b84996035bfd6
mkhan1231 <mahmud8118@gmail.com>
2018-12-23 13:39:16 -0500

    Permutation Solved

--- src/string/problems/Permutation.java
+++ src/string/problems/Permutation.java
@@ -12,6 +12,35 @@ public class Permutation {
          * Write Java program to compute all Permutation of a String
          *
          */
+        String str = "ABC";
+        int n = str.length();
+        System.out.println("Permutations of ABC are: ");
+        Permutation permutation = new Permutation();
+        permutation.permute(str, 0, n-1);
 
+    }
+    private void permute(String str, int l, int r)
+    {
+        if (l == r)
+            System.out.println(str);
+        else
+        {
+            for (int i = l; i <= r; i++)
+            {
+                str = swap(str,l,i);
+                permute(str, l+1, r);
+                str = swap(str,l,i);
+            }
+        }
+    }
+
+    public String swap(String a, int i, int j)
+    {
+        char temp;
+        char[] charArray = a.toCharArray();
+        temp = charArray[i] ;
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+        return String.valueOf(charArray);
     }
 }
