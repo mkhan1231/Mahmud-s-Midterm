7a3bdbd8bb6cfb2a7e071eca5afc99b9268fe31b
mkhan1231 <mahmud8118@gmail.com>
2018-12-23 15:34:26 -0500

    MakePyramid Solved

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -14,6 +14,15 @@ public class MakePyramid {
 
         */
 
-
+        for(int i=0;i<25;i++) {
+            for(int j=0;j<25-i;j++) {
+                System.out.print(" ");
+            }
+            for(int k=0;k<=i;k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
         }
-  }
+
+    }
+}
