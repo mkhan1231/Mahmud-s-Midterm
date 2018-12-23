e73e07605581be77c149ec62d993bd6fa983fd8d
mkhan1231 <mahmud8118@gmail.com>
2018-12-23 14:03:29 -0500

    Fibonacci SOlved

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -5,7 +5,16 @@ public class Fibonacci {
          /*
           Write 40 Fibonacci numbers with java.
          */
-
-
+        int fibCount = 40;
+        int[] fib = new int[fibCount];
+        fib[0] = 0;
+        fib[1] = 1;
+        for(int i=2; i < fibCount; i++){
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+        System.out.println("First 40 fibonacci numbers are shown vertically: ");
+        for(int i=0; i< fibCount; i++){
+            System.out.println(fib[i]);
+        }
     }
 }
