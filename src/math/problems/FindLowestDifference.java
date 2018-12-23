da64ee9738568123cd1b08ee90a75162894a2085
mkhan1231 <mahmud8118@gmail.com>
2018-12-23 15:04:05 -0500

    FindLowestDifference Solved

--- src/math/problems/FindLowestDifference.java
+++ src/math/problems/FindLowestDifference.java
@@ -1,5 +1,7 @@
 package math.problems;
 
+import java.util.Arrays;
+
 public class FindLowestDifference {
     public static void main(String[] args) {
         /*
@@ -7,12 +9,33 @@ public class FindLowestDifference {
          Read this below two array. Find the lowest difference between the two array cell.
          The lowest difference between cells is 1
         */
-        int [] array1 = {30,12,5,9,2,20,33,1};
-        int [] array2 = {18,25,41,47,17,36,14,19};
+        int[] array1 = {30, 12, 5, 9, 2, 20, 33, 1};
+        int[] array2 = {18, 25, 41, 47, 17, 36, 14, 19};
 
+        int m = array1.length;
+        int n = array2.length;
 
-
-
+        System.out.println("The Lowest Difference is : "+findSmallestDifference(array1, array2, m, n));
     }
 
+    static int findSmallestDifference(int A[], int B[], int m, int n) {
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int a = 0, b = 0;
+
+        int result = Integer.MAX_VALUE;
+
+        while (a < m && b < n)
+        {
+            if (Math.abs(A[a] - B[b]) < result)
+                result = Math.abs(A[a] - B[b]);
+            if (A[a] < B[b])
+                a++;
+            else
+                b++;
+        }
+        return result;
+    }
 }
+
+
