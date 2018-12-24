8eb6997939e0a3d24d87805ea413c6ae0bd2138f
mkhan1231 <mahmud8118@gmail.com>
2018-12-24 01:18:11 -0500

    Pattern Solved

--- src/databases/UnitTestConnectDB.java
+++ src/databases/UnitTestConnectDB.java
@@ -1,8 +1,17 @@
 package databases;
+import org.testng.Assert;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.lang.ClassNotFoundException;
 
 public class UnitTestConnectDB {
-    public static void main(String[] args) {
-        ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
+    public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException{
+        ConnectToSqlDB con = new ConnectToSqlDB();
+        Connection resultConnection=con.connectToSqlDatabase();
+        //Assert.assertEquals(resultConnection,
 
     }
 }
--- src/math/problems/Pattern.java
+++ src/math/problems/Pattern.java
@@ -8,13 +8,24 @@ public class Pattern {
 		 *
 		 *
 		 */
-		int n=100;
-		for(int i=n; i>=1; i--)
-		{
-			System.out.print(i+" , ");
+
+		//Using Nested loop
+
+		int num1 = 1;
+		int num2 = 100;
+		int num3 = 0;
+		int num4=0;
+		int[] a = new int[40];
+
+		while (num2 > 0) {
+			for (int i = 0; i < 10; i++) {
+				System.out.print(num2+",");
+				a[num3] = num2;
+				num2 = num2-num1;
+				num3++;
+			}
+			num1++;
 		}
-
-
 	}
 }
 
